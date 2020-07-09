06e7bb025e7bf27c4540ba076a9720fd6e5b8640
vloison <virg.loison@gmail.com>
2020-07-09 10:44:32 +0200

    network & params

--- network.py
+++ network.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 import torch as torch
 import torch.nn.functional as F
+from params import *
 from collections import OrderedDict
 
 
@@ -97,34 +98,6 @@ class RCNN(nn.Module):
         return output
 
 
-# PARAMETERS FOR THE FEATURE EXTRACTOR
-N_CONV_LAYERS = 7
-# Convolutional layers
-N_CONV_OUT = [64, 128, 256, 256, 512, 512, 512]
-CONV_KERNEL_SIZES = [3, 3, 3, 3, 3, 3, 2]
-CONV_STRIDES = [1, 1, 1, 1, 1, 1, 1]
-CONV_PADDINGS = [1, 1, 1, 1, 1, 1, 0]
-CONV = {
-    'kernel': CONV_KERNEL_SIZES,
-    'stride': CONV_STRIDES,
-    'padding': CONV_PADDINGS
-}
-# Batch normalization
-BATCH_NORM = [False, False, True, False, True, False, True]
-# Maxpooling
-MP_KERNEL_SIZES = [2, 2, 0, (2, 2), 0, (2, 2), 0]
-MP_STRIDES = [2, 2, 0, (2, 1), 0, (2, 1), 0]
-MP_PADDINGS = [0, 0, 0, (1, 1), 0, (1, 1), 0]
-MAX_POOL = {
-    'kernel': MP_KERNEL_SIZES,
-    'stride': MP_STRIDES,
-    'padding': MP_PADDINGS
-}
-# PARAMETERS FOR THE RECURRENT NETWORK
-N_REC_LAYERS = 2
-N_HIDDEN = 256
-N_CHARACTERS = 26
-
 
 if __name__ == "__main__":
     print('Example of usage')
@@ -140,6 +113,7 @@ if __name__ == "__main__":
                     n_r_layers=N_REC_LAYERS,
                     n_hidden=N_HIDDEN,
                     n_out=N_CHARACTERS, bidirectional=True)
+    # The arguments of RCNN are defined in params.py
     print('Network \n', fullrcnn)
 
     zbis = fullrcnn(x)
